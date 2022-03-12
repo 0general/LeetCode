f9c49fcceee3a8a3d660604eb424150bc102aae0
Hi YoungJin <56211221+0general@users.noreply.github.com>
2022-03-13 03:03:03 +0900

    Time: 709 ms (31.92%), Space: 40.8 MB (5.48%) - LeetHub

--- 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
+++ 128-longest-consecutive-sequence/128-longest-consecutive-sequence.py
@@ -5,17 +5,19 @@ class Solution:
         if self.parent[a] != a:
             self.parent[a] = self.find_parent(self.parent[a])
         return self.parent[a]
+    
+
         
     def longestConsecutive(self, nums: List[int]) -> int:
         number = {}
         
         for num in nums:
             number[num] = 1
-            if number.get(num-1,0): # parent 바꿔주기
+            if number.get(num-1,0):
                 self.parent[num] = self.find_parent(num-1)
             else:
                 self.parent[num] = num
-            if number.get(num+1,0): # 집합을 합치는 역할
+            if number.get(num+1,0):
                 self.parent[num+1] = self.parent[num]
         
         ans = 0
@@ -23,6 +25,3 @@ class Solution:
             ans = max(ans, num - self.find_parent(num)+1)
         
         return ans
-        
-        
-        
