95543443b9146ca04f929cf1e231ef75ced6b50b
Hi YJ <56211221+0general@users.noreply.github.com>
2022-06-19 22:28:55 +0900

    Time: 621 ms (18.76%), Space: 32.2 MB (12.75%) - LeetHub

---
+++ 456-132-pattern/456-132-pattern.py
@@ -0,0 +1,19 @@
+class Solution:
+    def find132pattern(self, nums: List[int]) -> bool:
+        if len(nums) < 3:
+            return False
+        stack = [] # 가장 큰 수가 맨 위로
+        comp = -1e9-1
+        for num in nums[::-1]:
+            if comp > num: # 큰 수 중에 제일 작은 수 보다 작은 수가 존재
+                return True
+            
+            while stack and stack[-1] < num:
+                comp = stack.pop() # 큰 수 중에 가장 작은 수 저장
+            stack.append(num)
+        
+        return False
+        
+        
+        
+        
