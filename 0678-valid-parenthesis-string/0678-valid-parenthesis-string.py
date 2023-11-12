4d6285e8fc531cf647f016d8bc9cae73a4522fac
Hi YJ <56211221+0general@users.noreply.github.com>
2023-11-12 21:04:20 +0900

    Time: 36 ms (75.14%), Space: 16.1 MB (73.75%) - LeetHub

--- 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.py
@@ -4,7 +4,7 @@ class Solution:
         for i in s:
             if i == "(" or i == "*":
                 left += 1
-            elif left > 0:
+            elif left:
                 left -=1
             else:
                 return False
@@ -13,7 +13,7 @@ class Solution:
         for i in s[::-1]:
             if i == ")" or i == "*":
                 right += 1
-            elif right > 0:
+            elif right:
                 right -=1
             else:
                 return False
